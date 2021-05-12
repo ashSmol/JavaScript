1211bab0c70b14435dfd71a2f42d912a5ba4ccb1
AshSmol <ashiryaev84@gmail.com>
2021-05-12 22:58:43 +0300

    1st task finnished

---
+++ scr1.js
@@ -0,0 +1,20 @@
+// задаем исходный массив, из которого будем исключать числа, имеющие делитель
+let arr = [];
+for (let i = 1; i <= 100; i++) {
+    arr.push(i)
+}
+
+let index = 0;
+while (index <= 100) {
+    let devider = 2;
+    while (devider < arr[index]) {
+        if (arr[index] % devider === 0) {
+            arr.splice(index, 1);
+            index--; // Т.к. индексы после удаления элемента сместились, нужно так же уменьшить счетчик 
+            break;
+        }
+        devider++;
+    }
+    index++;
+}
+console.log(arr);
